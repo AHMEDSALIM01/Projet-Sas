8a169215fa0801fb871379e5973e7763e221f3c8
AHMEDSALIM01 <salim.ahm01@gmail.com>
2021-11-23 16:52:35 +0100

    resoudre le probleme de la mise a jour du fichier apres l'operation executee

--- Projet/main.c
+++ Projet/main.c
@@ -14,6 +14,7 @@ struct insertc
 
  int search(char CINCH[]){
  	FILE *FI;
+ 	strupr(CINCH);
  	FI=fopen("gestion_bancaire.txt","r");
  	do
  	{
@@ -31,6 +32,7 @@ struct insertc
  	FILE *FI;
  	FI=fopen("gestion_bancaire.txt","a");
  	char CIN[10];
+ 	strupr(CIN);
  	printf("entrer le CIN :");
  	scanf("%s",&CIN);
  	while(search(CIN)==0){
@@ -45,7 +47,7 @@ struct insertc
  	fprintf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,i.anc_montant,i.nov_montant);
  	fflush(stdin);
  	fclose(FI);
- 	printf("*****le compte a ete cree avec succee*****\n");
+ 	printf("*****le compte a ete creer avec succees*****\n");
  	}
 
  void insert_ncmp(){
@@ -55,7 +57,7 @@ struct insertc
  	int j,n;
  	printf("combien de compte que vous devrez creer :");
  	scanf("%d",&n);
- 	while(n==0){printf("veuillez entrer le nombre des fois different a 0 :");
+ 	while(n>0){printf("veuillez entrer le nombre des fois different a 0 :");
  		scanf("%d",&n);
  		}
  	for(j=0;j<n;j++){
@@ -74,71 +76,132 @@ struct insertc
  	fprintf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,i.anc_montant,i.nov_montant);
  	fflush(stdin);}
  	fclose(FI);
- 	printf("*****le compte a ete cree avec succee*****\n");
+ 	printf("*****leS compte a etes creer avec succees*****\n");
  	}
 
  void retrait(){
 
- 	FILE *FI;
- 	FI=fopen("gestion_bancaire.txt","a");
+ 	FILE *FI,*tempo;
+ 	FI=fopen("gestion_bancaire.txt","r");
+ 	tempo=fopen("tmp.txt","w+");
  	char CIN[10];
+ 	strupr(CIN);
  	long a;
  	printf("entrer le CIN :");
  	scanf("%s",&CIN);
- 	if(search(CIN)!=0){
- 		printf ("le CIN %s n'existe pas merci d'introduire un compte\n",CIN);
- 		return;}
- 	else{
+ 	if(search(CIN)==1){
+        printf("le CIN n'existe pas merci d'introduire un compte\n");
+        return;
+    }
+ 	do{
+     fscanf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,&i.anc_montant,&i.nov_montant);
+ 	if(strcmp(CIN,i.CIN)==0){
  	i.anc_montant=i.nov_montant;
+ 	strcpy(i.CIN,CIN);
+ 	printf("Prenom: ");scanf("%s",&i.Prenom);
+ 	printf("Nom: ");scanf("%s",&i.Nom);
  	printf("donner le montant de retrait (DH)= ");
  	scanf("%ld",&a);
+ 	while(a<=0){
+        printf("donner le montant exacte :\n");
+        printf("donner le montant de retrait (DH)= ");
+        scanf("%ld",&a);
+ 	}
  	i.nov_montant-=a;
  	printf("votre ancien montant (DH)= %ld\n",i.anc_montant);
- 	fprintf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,i.anc_montant,i.nov_montant);
  	printf("votre noveau montant (DH)= %ld\n",i.nov_montant);
  	printf("\n");
+ 	}
+ 	fprintf(tempo,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,i.anc_montant,i.nov_montant);
+ 	}while(!feof(FI));
  	fflush(stdin);
  	fclose(FI);
+ 	fclose(tempo);
+ 	remove("gestion_bancaire.txt");
+ 	rename("tmp.txt","gestion_bancaire.txt");
  	printf("*****votre operation a ete effectuer avec succee*****\n");
- 	}
-
  }
+
+
+
+
 
  void depot(){
 
- 	FILE *FI;
- 	FI=fopen("gestion_bancaire.txt","a");
+ 	FILE *FI,*tempo;
+ 	FI=fopen("gestion_bancaire.txt","r");
+ 	tempo=fopen("tmp.txt","w+");
  	char CIN[10];
+ 	strupr(CIN);
  	long a;
  	printf("entrer le CIN :");
  	scanf("%s",&CIN);
- 	if(search(CIN)!=0){
- 		printf ("le CIN %s n'existe pas merci d'introduire un compte\n",CIN);
- 		return;}
- 	else{
+ 	if(search(CIN)==1){
+        printf("le compte de ce CIN n'existe pas merci d'introduire un compte\n");
+        return;
+    }
+ 	do{
+     fscanf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,&i.anc_montant,&i.nov_montant);
+ 	if(strcmp(CIN,i.CIN)==0){
  	i.anc_montant=i.nov_montant;
- 	printf("donner le montant de depot (DH)= ");
+ 	strcpy(i.CIN,CIN);
+ 	printf("Prenom: ");scanf("%s",&i.Prenom);
+ 	printf("Nom: ");scanf("%s",&i.Nom);
+ 	printf("donner le montant de retrait (DH)= ");
  	scanf("%ld",&a);
+ 	while(a<=0){
+        printf("donner le montant exacte :\n");
+        printf("donner le montant de retrait (DH)= ");
+        scanf("%ld",&a);
+ 	}
  	i.nov_montant+=a;
  	printf("votre ancien montant (DH)= %ld\n",i.anc_montant);
- 	fprintf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,i.anc_montant,i.nov_montant);
- 	printf("votre votre noveau montant (DH)= %ld\n",i.nov_montant);
+ 	printf("votre noveau montant (DH)= %ld\n",i.nov_montant);
  	printf("\n");
+ 	}
+ 	fprintf(tempo,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,i.anc_montant,i.nov_montant);
+ 	}while(!feof(FI));
  	fflush(stdin);
  	fclose(FI);
+ 	fclose(tempo);
+ 	remove("gestion_bancaire.txt");
+ 	rename("tmp.txt","gestion_bancaire.txt");
  	printf("*****votre operation a ete effectuer avec succee*****\n");
- 	}
 	}
+void aff_rechcin(){
+    FILE *FI;
+    FI=fopen("gestion_bancaire.txt","r");
+    char CIN[11];
+    printf("donner le CIN du compte souhaitee : ");
+    scanf("%s",CIN);
+    if(search(CIN)==0)
+    {
+       printf("\t=======Liste d'information=======\n");
+       do{
+        fscanf(FI,"%s |%s |%s |%ld |%ld \n",i.CIN,i.Prenom,i.Nom,&i.anc_montant,&i.nov_montant);
+        if(strcmp(i.CIN,CIN)==0){
+        printf("CIN :");printf("%s| ",i.CIN);printf("Prenom:");printf("%s| ",i.Prenom);printf("Nom:");printf("%s| ",i.Nom);printf("Anc.Montant(DH):");printf("%ld| ",i.anc_montant);printf("Nouv.Montnant(DH):");printf("%ld\n",i.nov_montant);
+        }
+       }while(!feof(FI));
+       fflush(stdin);
+       fclose(FI);
+    }
+    else
+    {
+       printf("le compte de ce CIN n'existe pas merci d'introduire un compte\n");
+    }
+}
 
 int main(){
 	int c,b;
 	retour:
-	printf("**********Meunu Principal********** \n");
+	printf("\n\n**********Meunu Principal********** \n");
 	printf("chosire: \n");
 	printf("1: introduire un compte\n");
 	printf("2: introduire plusieur comptes a la fois\n");
 	printf("3: Operations \n");
-	printf("4: Quitter \n");
+	printf("4: Recherche par CIN\n");
+	printf("5: Quitter \n");
 	scanf("%d",&b);
 	switch(b)
 	{case 1:insert_cmp();
@@ -158,8 +221,9 @@ int main(){
 				goto retour;
 			}
 			goto retour;
-
-	default:printf("***************BYE***************");
+     case 4:aff_rechcin();
+     break;
+	default:printf("\t\t***************BYE***************");
 	}
 
 	return 0;
